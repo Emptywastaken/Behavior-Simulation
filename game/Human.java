a5c3022d50f5a19c9e9def5fdd509f5ade5afaaf
Tommaso Cicchinelli <cicchinellitommaso@gmail.com>
2024-05-14 20:42:03 +0200

    removed old methods

--- game/Human.java
+++ game/Human.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 public abstract class Human extends Entity {
 
-     
     private boolean readyToReplicate = false;
     private boolean hasFood = false;
     private int turnsLeft;
@@ -12,8 +11,7 @@ public abstract class Human extends Entity {
     private final int vision;
     private final int speed;
 
-
-    public Human(Cell cell, int vision, int stregth, int turnsLeft, int speed){
+    public Human(Cell cell, int vision, int stregth, int turnsLeft, int speed) {
         super(cell);
         this.vision = vision;
         this.stregth = stregth;
@@ -24,57 +22,61 @@ public abstract class Human extends Entity {
     // Pick direction function depends on the character of the human
     // greedy, generous etc...
     public abstract Cell pickDirection();
+
     public abstract boolean toEat();
 
     // TODO: consider case when human needs to choose who gets the food.
-    public abstract boolean shareFood(); 
-    
+    public abstract boolean shareFood();
+
     // added in case we consider more factors when checking for replication
     public abstract void replicate();
-    
+
     public void Move(Cell new_cell) throws OutOfRangeException {
         int x = super.getX();
         int y = super.getX();
         double dist = Helper.getDistance(x, y, new_cell.getColumn(), new_cell.getRow());
-        
-        if(dist > speed) {
-            throw new OutOfRangeException();    
+
+        if (dist > speed) {
+            throw new OutOfRangeException();
         }
         super.ChangeCell(new_cell);
-    }   
+    }
 
     // returns a list of all cells with foods in the visible radius
-    // TODO check for tuples or smth similar in order to be able to priorities cells by direction 
+    // TODO check for tuples or smth similar in order to be able to priorities cells
+    // by direction
     public ArrayList<Cell> foodInVision() {
         // loops through each cell in the vision radius.
-        
+
         int positionX = super.getX();
         int positionY = super.getY();
 
         // in order to consider out of range cases.
-        int yStartingPoint = Math.min(positionY-vision, 0);
-        int yEndPoint = Math.max(positionY+vision, this.GetCell().boardHeight());
-        int xStartingPoint = Math.min(positionX-vision, 0);
-        int xEndPoint = Math.max(positionX+vision, this.GetCell().boardWidth());
+        int yStartingPoint = Math.min(positionY - vision, 0);
+        int yEndPoint = Math.max(positionY + vision, this.GetCell().getRow());
+        int xStartingPoint = Math.min(positionX - vision, 0);
+        int xEndPoint = Math.max(positionX + vision, this.GetCell().getColumn());
 
         Board BOARD = this.GetCell().getBoard();
         ArrayList<Cell> returnedCells = new ArrayList<Cell>();
         // need reference to board for this one
-        for (int i = yStartingPoint; i <= yEndPoint; i++) {   
-            for (int j= xStartingPoint; j <= xEndPoint; j++) {
+        for (int i = yStartingPoint; i <= yEndPoint; i++) {
+            for (int j = xStartingPoint; j <= xEndPoint; j++) {
                 Cell cell = BOARD.getCell(i, j);
 
-                if(cell.isEmpty()) {
+                if (cell.isEmpty()) {
                     continue;
-                } if(Helper.getDistance(i, positionX, j, positionY) > vision) {
+                }
+                if (Helper.getDistance(i, positionX, j, positionY) > vision) {
                     continue;
-                } if(cell.containsFood() != -1){
+                }
+                if (cell.containsFood() != -1) {
                     returnedCells.add(cell);
-                }     
+                }
             }
         }
         return returnedCells;
-    } 
+    }
 
     // returns all neighbouring players
     public ArrayList<Entity> getNeighboors() {
@@ -84,39 +86,40 @@ public abstract class Human extends Entity {
 
         Board BOARD = this.GetCell().getBoard();
         ArrayList<Entity> neighbours = new ArrayList<Entity>();
-        for (int i = y-1; i <= y+1; i++) {
-            for (int j = x-1; j < x; j++) {
+        for (int i = y - 1; i <= y + 1; i++) {
+            for (int j = x - 1; j < x; j++) {
 
-                if(i == x & j == y) {
+                if (i == x & j == y) {
                     continue;
                 }
-                if(!BOARD.cellInRange(i, j)) {
+                if (!BOARD.cellInRange(i, j)) {
                     continue;
                 }
 
                 Cell cell = BOARD.getCell(i, j);
                 if (!cell.isEmpty()) {
-                    for (Entity entitiy : cell.getElements()){
+                    for (Entity entitiy : cell.getElements()) {
                         if (entitiy instanceof Human) {
                             neighbours.add(entitiy);
                         }
                     }
 
-                }  
+                }
             }
         }
         return neighbours;
     }
 
-
     @Override
     public void death() {
-        if(turnsLeft <= 0) {
+        if (turnsLeft <= 0) {
             super.death();
         }
-	}
-    // TODO: randomness eg.(lambda*stregthDiffernce) chance to alternate the outcome.
-    public void Attack(Human target){
+    }
+
+    // TODO: randomness eg.(lambda*stregthDiffernce) chance to alternate the
+    // outcome.
+    public void Attack(Human target) {
         if (this.stregth == target.stregth) {
             this.decreaseHealth();
             target.decreaseHealth();
@@ -127,7 +130,7 @@ public abstract class Human extends Entity {
         }
     }
 
-    public void pickFood(){
+    public void pickFood() {
         // change player's status and removes food.
         int foodIndx = super.GetCell().containsFood();
         if (foodIndx != -1) {
@@ -135,17 +138,17 @@ public abstract class Human extends Entity {
             this.GetCell().GetElement(foodIndx).death();
         }
     }
-    
+
     public void decreaseHealth() {
         this.turnsLeft--;
-        if (turnsLeft<=0){
+        if (turnsLeft <= 0) {
             this.death();
         }
     }
 
     public void decreaseHealth(int amount) {
         this.turnsLeft = this.turnsLeft - amount;
-        if(turnsLeft<=0){
+        if (turnsLeft <= 0) {
             this.death();
         }
     }
